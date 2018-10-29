a0d149e707b59a034a85c118cf7f7a4f4c7055f7
Mike <continuousbutnotdifferentiable@gmail.com>
2018-10-29 13:55:53 -0700

    got rid of main adds catch and all tests for cache.hh

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### CS389 HW2: Hash it out.
+### CS389 HW3: Testy Cache.
 
 ####
 DAVID HERRERO
@@ -6,31 +6,3 @@ MICHAEL KALANGE
 
 #### IMPORTANT
 
-Compile using:
-
-`g++ -std=c++17 main.cpp`
-
-#### Part 2: Testing
-
-To test the two different evictors, `evictor_type_` must be changed in the `Impl` declaration. For FIFO evictor, use `"fifo"` and for LRU evictor, use `"lru"`. Also, uncomment the consequent evictor test in main.cpp and comment the other.
-
-#### Part 3: Performance
-
-We've implemented a universal hashing function for strings from the link(s): 
-https://en.wikipedia.org/wiki/Universal_hashing and
-http://www.cse.yorku.ca/~oz/hash.html
-
-It takes some prime number that is large relative to the length of the string, bit shifts it by 5, and adds a character to that result, looping over all characters in the key, then returns the resulting hash.
-
-#### Part 4: Collision resolution
-
-Unordered map uses buckets which actually correspond to iterators into singly linked lists of the objects 'in' each bucket. There's no reason to fix what isn't broken so we've decided to just let unordered_map do its thing.
-
-This link:
-https://stackoverflow.com/questions/31112852/how-stdunordered-map-is-implemented/31113618#31113618
-provides a stellar account of the mechanics involved in unordered_map's collision resolution.
-
-#### Part 6: Eviction policy
-
-Again, unordered map provides this functionality.
-We just initialize my_cache_'s max load factor to be .5, and the table does automatic resizing to handle loads above that limit.
--- tests_cache.cpp
+++ tests_cache.cpp
@@ -1,19 +1,10 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "cache.hh"
+#include "testinglab.cpp"
 
-TEST_CASE("Create cache set/get value.","[single-file]")
+TEST_CASE("Set/Get","[single-file]")
 {
-    const Cache::key_type key1 = "one";
-    const int val = 1;
-    const Cache::val_type point1 = &val;
-    const Cache::index_type size = sizeof(val);
-    Cache new_cache(1000);
-    Cache::index_type sized;
-    new_cache.set(key1, point1, size);
-    Cache::val_type thepoint = new_cache.get(key1, sized);
-    const uint32_t &theval = *(static_cast<const u_int32_t *>(thepoint));
-    SECTION("A value in the cache can be retrieved and its size is stored in the address.")
+    SECTION("A value in the cache can be retrieved.")
     {
         const Cache::key_type key1 = "one";
         const int val = 1;
@@ -27,9 +18,24 @@ TEST_CASE("Create cache set/get value.","[single-file]")
 
         // Get the value
         Cache::val_type the_point = new_cache.get(key1, sized);
-        const uint32_t &theval = *(static_cast<const u_int32_t *>(thepoint));
+        const uint32_t &theval = *(static_cast<const u_int32_t *>(the_point));
 
         REQUIRE(theval == val);
+    }
+    SECTION("The size of the value returned is correct.")
+    {
+        const Cache::key_type key1 = "one";
+        const int val = 1;
+        const Cache::val_type point1 = &val;
+        const Cache::index_type size = sizeof(val);
+        Cache new_cache(1000);
+        Cache::index_type sized;
+        
+        // Set the value
+        new_cache.set(key1, point1, size);
+
+        // Get the value
+        new_cache.get(key1, sized);
         REQUIRE(sized == size);
     }
     SECTION("Getting a value not in the cache returns null.")
@@ -40,5 +46,66 @@ TEST_CASE("Create cache set/get value.","[single-file]")
         
         Cache::val_type the_point = new_cache.get(key1, sized);
         REQUIRE(!the_point);
+    }
+}
+
+TEST_CASE("Space_used")
+{
+    SECTION("Ensure memused is 0 when no values are inserted.")
+    {
+        const Cache::key_type key1 = "one";
+        const int val = 1;
+        const Cache::val_type point1 = &val;
+        const Cache::index_type size = sizeof(val);
+        Cache new_cache(1000);
+        Cache::index_type sized;
+        
+        // Set the value
+        new_cache.set(key1, point1, size);
+
+        // Get memused
+        sized = new_cache.space_used();
+        REQUIRE(sized == size);
+    }
+}
+
+TEST_CASE("Del")
+{
+    SECTION("Delete removes the key/value from the cache.")
+    {
+        const Cache::key_type key1 = "one";
+        const int val = 1;
+        const Cache::val_type point1 = &val;
+        const Cache::index_type size = sizeof(val);
+        Cache new_cache(1000);
+        Cache::index_type sized = 0;
+            
+        // Set the value
+        new_cache.set(key1, point1, size);
+        new_cache.del(key1);
+        Cache::val_type the_point = new_cache.get(key1, sized);
+
+        REQUIRE(sized == 0);
+        REQUIRE(!the_point);
+    }
+    SECTION("Delete on k/v not in cache has no effect.")
+    {
+        const Cache::key_type key1 = "one";
+        const Cache::key_type key2 = "two";
+
+        const int val = 1;
+        const Cache::val_type point1 = &val;
+        const Cache::index_type size = sizeof(val);
+        Cache new_cache(1000);
+        Cache::index_type sized = size;
+            
+        // Set the value
+        new_cache.set(key1, point1, size);
+        new_cache.del(key2);
+        Cache::val_type the_point = new_cache.get(key1, sized);
+        const uint32_t &theval = *(static_cast<const u_int32_t *>(the_point));
+
+        REQUIRE(size == new_cache.space_used());
+        REQUIRE(theval == val);
     }
 }
